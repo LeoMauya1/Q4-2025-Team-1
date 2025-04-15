8e773a54756005994b79cc5e0971ccbd74dcc138
MUKILTEO\1717860 <https://github.com/LeoMauya1/Q4-2025-Team-1.git>
2025-04-15 13:29:50 -0700

    yes

--- 3D Template/Assets/Dylan/Plunger.cs
+++ 3D Template/Assets/Dylan/Plunger.cs
@@ -9,11 +9,12 @@ public class NewMonoBehaviourScript : MonoBehaviour
     public GameObject Toilet;
     public bool unClogged = false;
     public int counter = 0;
-
+    public GameObject cloth;
     private void Start()
     {
         mainCamera = Camera.main;
         CameraZDistance = mainCamera.WorldToScreenPoint(transform.position).z;
+        cloth.gameObject.SetActive(false);
     }
 
 
@@ -24,6 +25,30 @@ public class NewMonoBehaviourScript : MonoBehaviour
         transform.position = NewWorldPosition;
     }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        GetComponent<Rigidbody>().freezeRotation = false;
+       
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Toilet"))
+        {
+            counter++;
+
+            if(counter >= 10)
+            {
+                unClogged = true;
+            }
+
+            if (unClogged == true)
+            {
+                Debug.Log("wow");
+                cloth.gameObject.SetActive(true);
+            }
+        }
+    }
 
     // Update is called once per frame
     void Update()
