f5c538bcd94cc5ff8a2a039eeca6ed37f38e2cf6
1717854 <1717854@mukilteo.wednet.edu>
2025-04-22 13:26:54 -0700

    boooooo

--- 3D Template/Assets/Leo/systems/DragAndDrop.cs
+++ 3D Template/Assets/Leo/systems/DragAndDrop.cs
@@ -3,12 +3,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.EventSystems;
 
-public class DragAndDrop : MonoBehaviour, IPointerDownHandler
+public class DragAndDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler, IDropHandler
 {
+
+
+    private RectTransform m_rectTransform;
+    private void Awake()
+    {
+        m_rectTransform = GetComponent<RectTransform>();
+    }
+    public void OnBeginDrag (PointerEventData evedata)
+    {
+
+    }
+    public void OnEndDrag(PointerEventData evedata)
+    {
+
+    }
+    public void OnDrag(PointerEventData eventdata)
+    {
+        m_rectTransform.anchoredPosition += eventdata.delta;
+
+
+        // Find a the player and create a gameobject in the real world that will move in relativity to the itme in the canvas. 
+
+
+        //FindAnyObjectByType<playerInventory>().gameObject
+    }
 
     public void OnPointerDown(PointerEventData eventData)
     {
         Debug.Log("mouse click down");
     }
  
+    public void OnDrop(PointerEventData eventData)
+    {
+
+    }
 }
